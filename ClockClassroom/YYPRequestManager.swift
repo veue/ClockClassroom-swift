543e21319b7259645a1b9024211de6fc9e4c52f3
yuyunpeng <yuyunpeng@meizemeiyi.com>
2017-06-02 17:46:13 +0800

    Profile

--- ClockClassroom/YYPRequestManager.swift
+++ ClockClassroom/YYPRequestManager.swift
@@ -12,17 +12,19 @@ import Alamofire
 
 class YYPRequestManager: NSObject {
     
+    let urlString = "https://api.51shizhong.com/api_v9/"
+    
     func getRequest(parameters : [String : Any], success : @escaping (_ response : [String : AnyObject])->(), failture : @escaping (_ error : Error)->()) {
-        let urlString = "sdfdsfs"
         Alamofire.request(urlString, method: .get, parameters: [:], encoding: JSONEncoding.default).responseJSON { response in
                 debugPrint(response)
             switch response.result
             {
-                case .success(let value):
-                    success(value as! [String : AnyObject])
-                
-                case .failure(let error):
-                    failture(error)
+                case .success:
+                    if let value = response.result.value {
+                        success(value as! [String : AnyObject])
+                    }
+                case .failure:
+                    failture(response.result.error!)
             }
         }
     }
